ef860d84c7ef2829ce5cb2fa9431f272b79c8cf0
Ethan Carollo <etcarollo@gmail.com>
2025-10-02 11:42:46 +0200

    Feat : add some sounds (yes a feature.)

--- Assets/Scripts/Core/Scene/PanelController.cs
+++ Assets/Scripts/Core/Scene/PanelController.cs
@@ -1,5 +1,7 @@
 using Data.Sound;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace Core.Scene
 {
@@ -8,11 +10,57 @@ namespace Core.Scene
         [SerializeField] private UserInterfaceSoundData userInterfaceSoundData;
         [SerializeField] private AudioSource audioSource;
         
+        [SerializeField] private Button startGameButton;
+        [SerializeField] private Button exitGameButton;
+
+        private void Awake()
+        {
+            if (startGameButton != null)
+            {
+                startGameButton.onClick.AddListener(OnStartGameButtonClicked);
+                startGameButton.onClick.AddListener(ClickButton);
+                AddHoverSound(startGameButton);
+            }
+
+            if (exitGameButton != null)
+            {
+                exitGameButton.onClick.AddListener(OnExitGameButtonClicked);
+                exitGameButton.onClick.AddListener(ClickButton);
+                AddHoverSound(exitGameButton);
+            }
+        }
+
+        private void AddHoverSound(Button button)
+        {
+            var trigger = button.gameObject.AddComponent<EventTrigger>();
+            var entry = new EventTrigger.Entry
+            {
+                eventID = EventTriggerType.PointerEnter
+            };
+            entry.callback.AddListener((eventData) => HoverButton());
+            trigger.triggers.Add(entry);
+        }
+
         public void HoverButton()
         {
-            
+            audioSource.PlayOneShot(userInterfaceSoundData.hoverButtonAudioClip);
         }
-        
+
+        public void ClickButton()
+        {
+            audioSource.PlayOneShot(userInterfaceSoundData.clickButtonAudioClip);
+        }
+
+        private void OnStartGameButtonClicked()
+        {
+            StartGame();
+        }
+
+        private void OnExitGameButtonClicked()
+        {
+            QuitGame();
+        }
+
         public void StartGame()
         {
             SceneTransitor.Instance.LoadScene(1);
--- Assets/Scripts/Data/Sound/UserInterfaceSoundData.cs
+++ Assets/Scripts/Data/Sound/UserInterfaceSoundData.cs
@@ -5,6 +5,8 @@ namespace Data.Sound
     [CreateAssetMenu(fileName = "UserInterfaceSoundData", menuName = "Sound/UserInterfaceSoundData")]
     public class UserInterfaceSoundData : ScriptableObject
     {
+        [Header("Buttons Sound")]
         public AudioClip hoverButtonAudioClip;
+        public AudioClip clickButtonAudioClip;
     }
 }
