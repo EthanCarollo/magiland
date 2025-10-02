dbe533d7daca172cf7f74a8833b12a962ea8e9e2
Ethan Carollo <etcarollo@gmail.com>
2025-10-02 14:09:08 +0200

    Fix : remove infinite recursivity in EnemyBehaviour

--- Assets/Scripts/Core/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Core/Enemy/EnemyBehaviour.cs
@@ -1,27 +1,18 @@
-using System.ComponentModel;
 using Data.Enemy;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class EnemyBehaviour : MonoBehaviour
 {
     [Header("References & Data")]
     [SerializeField] private EnemyData enemy;
     [SerializeField] private float currentLife;
     private SpriteRenderer _spriteRenderer;
-    private Material material;
     
     [Header("World UI")]
     [SerializeField] private TextMeshProUGUI enemyNameText;
     [SerializeField] private Slider enemyLifeSlider;
-
-    void Awake()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        material = _spriteRenderer.material;
-    }
 
     void Start()
     {
@@ -32,6 +23,7 @@ public class EnemyBehaviour : MonoBehaviour
     public void TakeDamage(float damage)
     {
         currentLife -= damage;
+        UpdateUi();
         if (currentLife <= 0)
         {
             Death();
@@ -43,12 +35,10 @@ public class EnemyBehaviour : MonoBehaviour
         enemyLifeSlider.maxValue = enemy.maxLife;
         enemyLifeSlider.value = currentLife;
         enemyNameText.text = enemy.enemyName;
-        UpdateUi();
     }
 
     void Death()
     {
         Debug.Log("Enemy is now dead");
-        // material.SetTexture("_BaseMap", enemy.deadBody.texture);
     }
 }
