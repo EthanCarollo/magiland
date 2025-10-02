a124930094f5711c4385f16e3010029936dfcaf3
Ethan Carollo <118255407+EthanCarollo@users.noreply.github.com>
2025-10-02 22:56:40 +0200

    Feat : add quest uiwith event

--- Assets/Scripts/Core/Controllers/BusQuestController.cs
+++ Assets/Scripts/Core/Controllers/BusQuestController.cs
@@ -6,17 +6,41 @@ namespace Core.Quest
     {
         public delegate void QuestEnd();
         public event QuestEnd OnQuestEnd;
+        
+        public delegate void QuestAdvancement(int progress, int maximum);
+        public event QuestAdvancement OnQuestAdvancement;
 
         // Serializer for the debug just
-        [SerializeField] private int actualKilledEnemy = 0;
-        [SerializeField] private int maxKilledEnemy = 10;
+        private int _actualKilledEnemy = 0;
+        public int actualKilledEnemy { get => _actualKilledEnemy; }
+        private int _maxKilledEnemy = 10;
+        public int maxKilledEnemy { get => _maxKilledEnemy; }
+        [SerializeField] private bool questEnd = false;
 
+        /**
+         * If you read this, I know I actually just can add a line in my condition on line 43, I
+         * just wanted to see if you would really read this boring code
+         *
+         * and make it, maybe, a little bit fun for your eyes.
+         */
+        public void Start()
+        {
+            OnQuestEnd += EndQuest;
+        }
+
+        public void EndQuest()
+        {
+            OnQuestEnd -= EndQuest;
+            questEnd = true;
+        }
+        
         public void OnEnemyDied()
         {
-            actualKilledEnemy++;
-            if (actualKilledEnemy >= maxKilledEnemy)
+            if (questEnd == false)
             {
-                OnQuestEnd?.Invoke();
+                _actualKilledEnemy++;
+                OnQuestAdvancement?.Invoke(actualKilledEnemy, maxKilledEnemy);
+                if (actualKilledEnemy >= maxKilledEnemy) OnQuestEnd?.Invoke();
             }
         }
     }
---
+++ Assets/Scripts/Core/UserInterface/BusQuestInterface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Core.Quest;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.UserInterface
+{
+    public class BusQuestInterface : MonoBehaviour
+    {
+        [SerializeField] private GameObject busQuestContainer;
+        [SerializeField] private TextMeshProUGUI busQuestDescription;
+        [SerializeField] private Slider busQuestProgressSlider;
+
+        private void Start()
+        {
+            busQuestDescription.text =
+                $"Fau unlck lbus, et pr ça fau tuer {BusQuestController.Instance.maxKilledEnemy} mec";
+            busQuestProgressSlider.value = 0;
+            busQuestProgressSlider.maxValue = BusQuestController.Instance.maxKilledEnemy;
+            if (BusQuestController.Instance != null)
+                BusQuestController.Instance.OnQuestAdvancement += UpdateUI;
+            
+        }
+
+        public void OnDisable()
+        {
+            if (BusQuestController.Instance != null)
+                BusQuestController.Instance.OnQuestAdvancement -= UpdateUI;
+        }
+
+        public void UpdateUI(int actualProgress, int maxProgress)
+        {
+            busQuestProgressSlider.value = maxProgress;
+            busQuestProgressSlider.value = actualProgress;
+        }
+    }
+}
