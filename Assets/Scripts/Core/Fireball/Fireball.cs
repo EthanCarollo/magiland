4e8c425f4a617ec999eb99c2bc226cd704366a9a
Ethan Carollo <118255407+EthanCarollo@users.noreply.github.com>
2025-10-03 00:40:15 +0200

    Feat : add Fireball Launcher

---
+++ Assets/Scripts/Core/Fireball/Fireball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Core.Fireball
+{
+    public class Fireball : MonoBehaviour
+    {
+        public float speed = 20f;
+        private Vector3 direction;
+
+        public void Launch(Vector3 targetPosition)
+        {
+            direction = (targetPosition - transform.position).normalized;
+            Debug.Log(targetPosition);
+        }
+
+        void Update()
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Fireball/FireballLauncher.cs
@@ -0,0 +1,36 @@
+﻿using Core.Controllers;
+using UnityEngine;
+
+namespace Core.Fireball
+{
+    public class FireballLauncher : MonoBehaviour
+    {
+        public GameObject fireballPrefab;
+        public float launchInterval = 2f;
+
+        private float timer;
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer >= launchInterval)
+            {
+                LaunchFireball();
+                timer = 0f;
+            }
+        }
+
+        public void LaunchFireball()
+        {
+            if (fireballPrefab != null && PlayerController.Instance != null)
+            {
+                GameObject fireballObj = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+                Fireball fireball = fireballObj.GetComponent<Fireball>();
+                if (fireball != null)
+                {
+                    fireball.Launch(PlayerController.Instance.transform.position);
+                }
+            }
+        }
+    }
+}
