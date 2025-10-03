046936d7dc19fbf919191c3bb0e3dc37fbedf846
Ethan Carollo <etcarollo@gmail.com>
2025-10-03 14:17:03 +0200

    WIP : make SceneTransitor work on every scene

--- Assets/Scripts/Core/Scene/SceneTransitor.cs
+++ Assets/Scripts/Core/Scene/SceneTransitor.cs
@@ -3,24 +3,13 @@ using UnityEngine;
 
 namespace Core.Scene
 {
-    public class SceneTransitor : MonoBehaviour
+    public class SceneTransitor : BaseController<SceneTransitor>
     {
-        public static SceneTransitor Instance;
         
         public delegate void LoadNewScene();
         public LoadNewScene OnLoadNewScene;
     
         public GameObject loadingScreen;
-
-        public void Awake()
-        {
-            if (Instance != null && Instance != this)
-            {
-                Destroy(this.gameObject);
-            }
-            Instance = this;
-            DontDestroyOnLoad(this);
-        }
     
         public void LoadScene(int sceneToLoad){
             OnLoadNewScene?.Invoke();
